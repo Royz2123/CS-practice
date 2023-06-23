e13f6a5b26c76f658ccf34fba29fb3ba03a8eb50
Roy Zohar <royzohar25@gmail.com>
2023-06-23 09:03:01 +0300

    Restrcutured project to adapt to streamlit app

---
+++ exercises/ExerciseTemplate/ExerciseTemplate.java
@@ -0,0 +1,21 @@
+/**
+ * Exercise template
+ *
+ */
+public class ExerciseTemplate {
+
+    /**
+     * (In case the method declaration is provided in the exercise) 
+     *
+     * @param param1 the first parameter
+     * @return the return value
+     **/
+    public static void templateMethod(double param1) {
+        // Fill in your solution here
+    }
+	
+	public static void main(String[] args) {
+		// Run your solution here
+	}
+
+}
---
+++ exercises/Exercise_2_1/Exercise_2_1.java
@@ -0,0 +1,22 @@
+/**
+ * Exercise 2.1 - Find the area of the enclosing circle given an edge length
+ *
+ */
+public class Exercise_2_1 {
+
+    /**
+     * Compute the enclosing circles' area
+     *
+     * @param squareEdgeLength the length of any edge in the square
+     * @return the enclosing circles' area
+     **/
+    public static double enclosingCircleArea(double squareEdgeLength) {
+        // Fill in your solution here
+    	return -1.0;
+    }
+	
+	public static void main(String[] args) {
+		// Run your solution here
+	}
+
+}
